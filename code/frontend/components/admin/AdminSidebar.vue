ab6735a1c5351b0ad43afad486802d7cc4d9b364
Kanyapat <kanyapat.c@kkumail.com>
2026-02-16 21:03:51 +0700

    fix /admin/blacklists path

--- code/frontend/components/admin/AdminSidebar.vue
+++ code/frontend/components/admin/AdminSidebar.vue
@@ -15,7 +15,7 @@
                     <span class="sidebar-text">User Management</span>
                 </NuxtLink>
 
-                <NuxtLink to="/admin/blacklist"
+                <NuxtLink to="/admin/blacklists"
                     class="sidebar-item flex items-center gap-3 px-3 py-2.5 text-gray-700 rounded-lg hover:bg-blue-50">
                     <i class="w-6 text-lg text-center text-gray-500 fas fa-user"></i>
                     <span class="sidebar-text">Blacklist Management</span>
