a3cd2ef19ffe1aac539203e7fe6a655ff058c2c9
ThanawatU <thanawat.u@kkumail.com>
2026-02-17 23:58:57 +0700

    Modified index.js

--- code/backend/src/routes/index.js
+++ code/backend/src/routes/index.js
@@ -7,8 +7,8 @@ const driverVerifRoutes = require('./driverVerification.routes');
 const bookingRoutes = require('./booking.routes');
 const notificationRoutes = require('./notification.routes')
 const mapRoutes = require('./maps.routes')
-const monitorRoutes = require('./monitor.routes');
 const { prisma } = require("../utils/prisma"); // adjust path if needed
+
 
 const router = express.Router();
 
@@ -20,10 +20,13 @@ router.use('/driver-verifications', driverVerifRoutes);
 router.use('/bookings', bookingRoutes);
 router.use('/notifications', notificationRoutes);
 router.use('/api/maps', mapRoutes);
+
+//เพิ่ม monitor routes
+const monitorRoutes = require('./monitor.routes');
 router.use('/monitor', monitorRoutes);
 
 // เพิ่ม blacklist routes
+const blacklistRoutes = require('./blacklist.routes');
 router.use('/blacklists', blacklistRoutes);
-
 
 module.exports = router;
